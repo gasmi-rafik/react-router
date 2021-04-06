786a414a0a5ece8597c1fdcf791fdaf87f99411a
rafik gasmi <gasmi.rafik.rg@gmail.com>
2021-04-06 09:57:28 +0100

    react roter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import React from "react";
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/5">About</Link>
+            </li>
+            <li>
+              <Link to={{
+                pathname :"/users" ,
+                state : {users :{ name:"jhon" ,
+                lastname : "doe"}}}}>
+                  Users
+                  </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about/:id" component={About } />
+        
+          <Route path="/users" render={(props)=>(<> <Users {...props} /> </>) }/>
+            
+        
+
+        </Switch>
+      </div>
+
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About(props) {
+  let x = props.history
+  console.log(props.match.params.id)
+  return<> 
+        <h2>About</h2>
+        <button onClick={()=>x.goBack()}>goback</button>
+        <button onClick={()=>x.push('./')}> push</button>
+        </>
+}
+
+function Users(props) {
+  console.log(props.location.state.users.name)
+  return <h2>Users</h2>;
+}
